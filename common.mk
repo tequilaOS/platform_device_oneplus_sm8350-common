4524186e9aed75fb29f6dc017b8a3a2e6b41a46f
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-09-27 15:06:36 +0200

    sm8350-common: Time for tequila!

--- common.mk
+++ common.mk
@@ -308,7 +308,7 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-tequila
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
